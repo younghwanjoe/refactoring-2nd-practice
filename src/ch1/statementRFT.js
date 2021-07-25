fb99d6eb53ca4b9cb2771fbc9f1d24ad5951c8db
younghwanjoe <hhgman2@gmail.com>
2021-07-25 17:30:54 +0900

    volumeCredits 변수를 인라인

--- src/ch1/statementRFT.js
+++ src/ch1/statementRFT.js
@@ -63,6 +63,6 @@ export function statement(invoice, plays) {
     return result; // 함수 안에서 값이 바뀌는 변수 반환
   }
   result += `총액: ${usd(totalAmount)}\n`;
-  result += `적립 포인트: ${volumeCredits}점\n`;
+  result += `적립 포인트: ${totalVolumeCredits()}점\n`;
   return result;
 }
