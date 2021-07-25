dea21e203feb99dba2dffd6368ad492dd59b8170
younghwanjoe <hhgman2@gmail.com>
2021-07-25 17:18:57 +0900

    적립 포인트 계산 코드를 volumeCreditsFor로 추출

--- src/ch1/statementRFT.js
+++ src/ch1/statementRFT.js
@@ -11,16 +11,21 @@ export function statement(invoice, plays) {
   for (let perf of invoice.performances) {
     let thisAmount = amountFor(perf, playFor(perf));
     // 포인트를 적립한다.
-    volumeCredits += Math.max(perf.audience - 30, 0);
-    // 희극 관객 5명마다 추가 포인트를 제공한다.
-    if ("comedy" === playFor(perf).type)
-      volumeCredits += Math.floor(perf.audience / 5);
-
+    // 추출한 함수를 이용해 값을 누적
+    volumeCredits += volumeCreditsFor(perf);
     // 청구 내역을 출력한다.
     result += `  ${playFor(perf).name}: ${format(thisAmount / 100)} (${
       perf.audience
     }석)\n`;
     totalAmount += thisAmount;
+  }
+
+  function volumeCreditsFor(perf) {
+    let volumeCredits = 0;
+    volumeCredits += Math.max(perf.audience - 30, 0);
+    if ("comedy" === playFor(perf).type)
+      volumeCredits += Math.floor(perf.audience / 5);
+    return volumeCredits;
   }
 
   function playFor(aPerformance) {
