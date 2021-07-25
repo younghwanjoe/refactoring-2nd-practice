0c6669ed317f4aa8b7c80e7600b17013c2cd610a
younghwanjoe <hhgman2@gmail.com>
2021-07-25 18:17:35 +0900

    renderPlainText()의 중첩 함수였던 amountFor()를 statement()로 옮김

--- src/ch1/statementRFT.js
+++ src/ch1/statementRFT.js
@@ -6,10 +6,34 @@ export function statement(invoice, plays) {
 
   function enrichPerformance(aPerformance) {
     const result = Object.assign({}, aPerformance);
-    result.play = playFor(result); // 중간 데이터에 연극 정보를 저장
+    result.play = playFor(result);
+    result.amount = amountFor(result);
     return result;
   }
-  // renderPlainText()의 중첩 함수였던 playFor()를 statement()로 옮김
+
+  function amountFor(aPerformance) {
+    let result = 0;
+
+    switch (aPerformance.play.type) {
+      case "tragedy": // 비극
+        result = 40000;
+        if (aPerformance.audience > 30) {
+          result += 1000 * (aPerformance.audience - 30);
+        }
+        break;
+      case "comedy": // 희극
+        result = 30000;
+        if (aPerformance.audience > 20) {
+          result += 10000 + 500 * (aPerformance.audience - 20);
+        }
+        result += 300 * aPerformance.audience;
+        break;
+      default:
+        throw new Error(`알 수 없는 장르: ${aPerformance.play.type}`);
+    }
+    return result; // 함수 안에서 값이 바뀌는 변수 반환
+  }
+
   function playFor(aPerformance) {
     return plays[aPerformance.playID];
   }
@@ -20,15 +44,13 @@ function renderPlainText(data, plays) {
 
   for (let perf of data.performances) {
     // 청구 내역을 출력한다.
-    result += `  ${perf.play.name}: ${usd(amountFor(perf, data))} (${
-      perf.audience
-    }석)\n`;
+    result += `  ${perf.play.name}: ${usd(perf.amount)} (${perf.audience}석)\n`;
   }
 
   function totalAmount() {
     let totalAmount = 0;
     for (let perf of data.performances) {
-      totalAmount += amountFor(perf);
+      totalAmount += perf.amount;
     }
     return totalAmount;
   }
@@ -57,28 +79,6 @@ function renderPlainText(data, plays) {
     return result;
   }
 
-  function amountFor(aPerformance) {
-    let result = 0;
-
-    switch (aPerformance.play.type) {
-      case "tragedy": // 비극
-        result = 40000;
-        if (aPerformance.audience > 30) {
-          result += 1000 * (aPerformance.audience - 30);
-        }
-        break;
-      case "comedy": // 희극
-        result = 30000;
-        if (aPerformance.audience > 20) {
-          result += 10000 + 500 * (aPerformance.audience - 20);
-        }
-        result += 300 * aPerformance.audience;
-        break;
-      default:
-        throw new Error(`알 수 없는 장르: ${aPerformance.play.type}`);
-    }
-    return result; // 함수 안에서 값이 바뀌는 변수 반환
-  }
   result += `총액: ${usd(totalAmount())}\n`;
   result += `적립 포인트: ${totalVolumeCredits()}점\n`;
   return result;
